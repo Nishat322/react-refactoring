bacc45bd416d18792aacdb65977e3bf41519d16e
Nishat Hoque <nishat.dev22@gmail.com>
2020-12-06 22:26:21 -0500

    final refactioring

---
+++ src/Cart.js
@@ -0,0 +1,39 @@
+import React from 'react'
+
+import CartTotal from './CartTotal'
+
+const USCurrencyFormat = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD'
+  });
+
+export default class Cart extends React.Component{
+    render(){
+        const summary = Object.keys(this.props.selected).map((feature, idx) => {
+            const featureHash = feature + '-' + idx;
+            const selectedOption = this.props.selected[feature];
+      
+            return (
+              <div className="summary__option" key={featureHash}>
+                <div className="summary__option__label">{feature} </div>
+                <div className="summary__option__value">{selectedOption.name}</div>
+                <div className="summary__option__cost">
+                  {USCurrencyFormat.format(selectedOption.cost)}
+                </div>
+              </div>
+            );
+          });
+        
+        return(
+            <div>
+            <section className="main__summary">
+                <h2>Your cart</h2>
+                {summary}
+                <CartTotal
+                selected = {this.props.selected}
+                />
+              </section>
+            </div>
+        )
+    }
+}
---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default class Header extends React.Component{
+    render(){
+        return (
+        <div className="Header">   
+            <header>
+                <h1>ELF Computing | Laptops</h1>
+            </header>
+        </div>
+        )
+    }
+}
